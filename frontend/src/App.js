a2f854a22f5e66192e0fd923bcee12a2a6dd6da2
Prathik Reddy <tvkp333555@gmail.com>
2021-11-14 18:05:43 +0530

    created navbar with routes and NavLinks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import {Routes,Route} from "react-router-dom";
 
-function App() {
+const App = ()=> {
   return (
     <>
     <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
     </>
-
-
   );
 }
-
 export default App;
+
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const SignUp = () => {
+const Login = () => {
     return (
         <div>
-            <h1>Signup page</h1>
+            <h1>login page</h1>
         </div>
     )
 }
 
-export default SignUp
+export default Login
