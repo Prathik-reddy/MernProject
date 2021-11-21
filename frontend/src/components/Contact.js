27ee24dbefd471221d650761b5b8a72ff23fd31a
Prathik Reddy <tvkp333555@gmail.com>
2021-11-21 14:10:21 +0530

    created home page

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -3,7 +3,27 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <h1>Contact page</h1>
+            <div className="w-100  text-center">
+            <div className="container">
+                <h1 className="p-5">Contact Us</h1>
+                <form>
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 m-2">
+                            <input type="text" class="form-control" placeholder="Name" />
+                        </div>
+                        <div class="col-md-6 m-2">
+                            <input type="email" class="form-control" placeholder="Email" />
+                        </div>
+                        <div class="col-md-6 m-2">
+                            <input type="text" class="form-control" placeholder="Your Message" />
+                        </div>
+                        <div class="col-md-6 m-2">
+                            <input type="submit" class="form-control bg-primary" value="Contact Us" />
+                        </div>
+                    </div>
+                </form>
+            </div>
+            </div>
         </div>
     )
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>home pg</h1>
+        <div className="container">
+                <div className="d-flex flex-column align-items-center justify-content-center" style={{"color":"red","height":"80vh"}}>
+                    <h3>Welcome</h3>
+                    <h1>We are the MERN developers</h1>
+                </div>
         </div>
     )
 }
